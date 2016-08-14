86bf12d91b9181f13739d17b3879d5155295d5b9
J27 <jean.d.marchal@gmail.com>
2016-08-14 14:56:11 +0200

    Misc

--- tests/test.R
+++ tests/test.R
@@ -31,31 +31,31 @@ library(SpaDES)
 
 ## RasterLayer
   ## 1 var
-    mySim <- simInit(
-      times = list(start = 1, end = 1, timeunit = "year"),
-      modules = list("fireSense_FrequencyPredict"),
-      paths = list(modulePath = " # replace with empty string instead"),
-      params = list(fireSense_FrequencyPredict = list(mapping = c(MDC_JUL = "MDC_JUN"))), ## An example of mapping
-      inputs = data.frame(
-        files = c("Z:/fireSense_FrequencyFitted_P.rds", "Z:/MDC_JUN.tif"),
-        functions = c("readRDS", "raster"),
-        package = c("base", "raster"),
-        objectName = c("fireSense_FrequencyFitted", NA),
-        stringsAsFactors = FALSE)
-    )
-
-  ## 6 var
     # mySim <- simInit(
     #   times = list(start = 1, end = 1, timeunit = "year"),
     #   modules = list("fireSense_FrequencyPredict"),
     #   paths = list(modulePath = " # replace with empty string instead"),
+    #   params = list(fireSense_FrequencyPredict = list(mapping = c(MDC_JUL = "MDC_JUN"))), ## An example of mapping
     #   inputs = data.frame(
-    #     files = c("Z:/fireSense_FrequencyFitted_NB.rds", "Z:/MDC_JUN.tif", "Z:/HW.tif", "Z:/HW.tif", "Z:/HW.tif", "Z:/HW.tif"),
-    #     functions = c("readRDS", "raster", "raster", "raster", "raster", "raster"),
-    #     package = c("base", "raster", "raster", "raster", "raster", "raster"),
-    #     objectName = c("fireSense_FrequencyFitted", "MDC_JUL", "HW", "CN", "DIST", "O"),
+    #     files = c("Z:/fireSense_FrequencyFitted_P.rds", "Z:/MDC_JUN.tif"),
+    #     functions = c("readRDS", "raster"),
+    #     package = c("base", "raster"),
+    #     objectName = c("fireSense_FrequencyFitted", NA),
     #     stringsAsFactors = FALSE)
     # )
+
+  ## 6 var
+    mySim <- simInit(
+      times = list(start = 1, end = 1, timeunit = "year"),
+      modules = list("fireSense_FrequencyPredict"),
+      paths = list(modulePath = " # replace with empty string instead"),
+      inputs = data.frame(
+        files = c("Z:/fireSense_FrequencyFitted_NB.rds", "Z:/MDC_JUN.tif", "Z:/HW.tif", "Z:/HW.tif", "Z:/HW.tif", "Z:/HW.tif"),
+        functions = c("readRDS", "raster", "raster", "raster", "raster", "raster"),
+        package = c("base", "raster", "raster", "raster", "raster", "raster"),
+        objectName = c("fireSense_FrequencyFitted", "MDC_JUL", "HW", "CN", "DIST", "O"),
+        stringsAsFactors = FALSE)
+    )
 
 spades(mySim, debug = FALSE)
 
