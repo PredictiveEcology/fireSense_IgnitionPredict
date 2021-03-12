a96d618a9c887cf1cbee074eb0c73b2b779b219e
Ian Eddy <ianmseddy@gmail.com>
2021-03-12 14:28:22 -0800

    big reorganization of module

--- fireSense_IgnitionPredict.R
+++ fireSense_IgnitionPredict.R
@@ -14,34 +14,12 @@ defineModule(sim, list(
   documentation = list("README.txt", "fireSense_IgnitionPredict.Rmd"),
   reqdPkgs = list("magrittr", "raster"),
   parameters = rbind(
-    defineParameter(name = "modelObjName", class = "character",
-                    default = "fireSense_IgnitionFitted",
-                    desc = "name of the object of class fireSense_IgnitionFit
-                            describing the statistical model used for
-                            predictions."),
-    defineParameter(name = "data", class = "character",
-                    default = "dataFireSense_IgnitionPredict",
-                    desc = "a character vector indicating the names of objects
-                            in the `simList` environment in which to look for
-                            variables present in the model formula. `data`
-                            objects can be data.frames, RasterLayers, RasterStacks or
-                            RasterBricks. However, data.frames cannot be mixed
-                            with objects of other classes."),
-    defineParameter(name = "mapping", class = "character, list", default = NULL,
-                    desc = "optional named vector or list of character strings
-                            mapping one or more variables in the model formula
-                            to those in `data` objects."),
     defineParameter("rescaleFactor", "numeric", (250 / 10000)^2,
-                    desc = "rescale predicted rates of fire counts at any given
-                            temporal and spatial resolutions by a factor
-                            `rescaleFactor = new_res / old_res`. `rescaleFactor`
-                            is the ratio between the data aggregation scale used
-                            for model fitting and the scale at which predictions
-                            are to be made. fireSense_IgnitionFit was fitted using
-                            the 10km resolution. If predictions are made at the 250m
-                            resolution (default here) we convert it in this way"),
-    defineParameter(name = ".runInitialTime", class = "numeric",
-                    default = start(sim),
+                    desc = paste("rescale predicted rates of fire counts at any given temporal and spatial",
+                                 "resolutions by a factor `rescaleFactor = new_res / old_res`.",
+                                 "`rescaleFactor` is the ratio between the data aggregation scale used",
+                                 "for model fitting and the scale at which predictions are to be made")),
+    defineParameter(name = ".runInitialTime", class = "numeric", default = start(sim),
                     desc = "when to start this module? By default, the start
                             time of the simulation."),
     defineParameter(name = ".runInterval", class = "numeric", default = 1,
@@ -54,23 +32,16 @@ defineModule(sim, list(
     defineParameter(".useCache", "logical", FALSE, NA, NA, "Should this entire module be run with caching activated? This is generally intended for data-type modules, where stochasticity and time are not relevant")
   ),
   inputObjects = rbind(
-    expectsInput(
-      objectName = "fireSense_IgnitionFitted",
-      objectClass = "fireSense_IgnitionFit",
-      sourceURL = NA_character_,
-      desc = "An object of class fireSense_IgnitionFit created with the fireSense_IgnitionFit module."
-    ),
-    expectsInput(
-      objectName = "dataFireSense_IgnitionPredict",
-      objectClass = "data.frame, RasterLayer, RasterStack",
-      sourceURL = NA_character_,
-      desc = "One or more objects of class data.frame, RasterLayer or RasterStack in which to look for variables with which to predict."
-    )
+    expectsInput(objectName = "fireSense_IgnitionFitted", objectClass = "fireSense_IgnitionFit",
+                 desc = "An object of class fireSense_IgnitionFit created with the fireSense_IgnitionFit module."),
+    expectsInput(objectName = "fireSense_IgnitionAndEscapeCovariates", objectClass = "data.frame",
+                 desc = "An object of class RasterStack or data.frame with prediction variables"),
+    expectsInput(objectName = "flammableRTM", objectClass = "RasterLayer",
+                 desc = "a raster with values of 1 for every flammable pixel")
   ),
-  outputObjects = createsOutput(
-    objectName = "fireSense_IgnitionPredicted",
-    objectClass = NA_character_,
-    desc = "An object whose class depends on that of the inputs, could be a RasterLayer or a vector of type numeric."
+  outputObjects = rbind(
+    createsOutput(objectName = "fireSense_IgnitionProbRaster", objectClass = "RasterLayer",
+                  desc = "a raster layer of ignition probabilities")
   )
 ))
 
@@ -81,18 +52,18 @@ doEvent.fireSense_IgnitionPredict = function(sim, eventTime, eventType, debug = 
   switch(
     eventType,
     init = {
-      sim <- scheduleEvent(sim, eventTime = P(sim)$.runInitialTime, moduleName, "run", eventPriority = 5.12)
+      sim <- scheduleEvent(sim, eventTime = P(sim)$.runInitialTime, moduleName, "run", eventPriority = 5.11)
 
       if (!is.na(P(sim)$.saveInitialTime))
         sim <- scheduleEvent(sim, P(sim)$.saveInitialTime, moduleName, "save", .last())
     },
     run = {
-      sim <- frequencyPredictRun(sim)
+      sim <- IgnitionPredictRun(sim)
       if (!is.na(P(sim)$.runInterval))
-        sim <- scheduleEvent(sim, time(sim) + P(sim)$.runInterval, moduleName, "run", eventPriority = 5.12)
+        sim <- scheduleEvent(sim, time(sim) + P(sim)$.runInterval, moduleName, "run", eventPriority = 5.11)
     },
     save = {
-      sim <- frequencyPredictSave(sim)
+      sim <- IgnitionPredictSave(sim)
 
       if (!is.na(P(sim)$.saveInterval))
         sim <- scheduleEvent(sim, time(sim) + P(sim)$.saveInterval, moduleName, "save", .last())
@@ -103,82 +74,85 @@ doEvent.fireSense_IgnitionPredict = function(sim, eventTime, eventType, debug = 
   invisible(sim)
 }
 
-frequencyPredictRun <- function(sim) {
-  moduleName <- current(sim)$moduleName
-  if (all(!is(sim[[P(sim)$modelObjName]], "fireSense_IgnitionFit"), !is(sim[[P(sim)$modelObjName]], "fireSense_FrequencyFit"))){
-    stop(moduleName, "> '", P(sim)$modelObjName, "' should be of class 'fireSense_IgnitionFit' or 'fireSense_FrequencyFit'")
-}
+IgnitionPredictRun <- function(sim) {
 
-  ## Toolbox: set of functions used internally by frequencyPredictRun
-    frequencyPredictRaster <- function(model, data, sim) {
-      model %>%
-        model.matrix(c(data, sim[[P(sim)$modelObjName]]$knots)) %>%
-        `%*%` (sim[[P(sim)$modelObjName]]$coef) %>%
-        drop %>% sim[[P(sim)$modelObjName]]$family$linkinv(.) %>%
-        `*` (P(sim)$rescaleFactor)
-    }
+ moduleName <- currentModule(sim)
+ fireSense_IgnitionCovariates <- copy(sim$fireSense_IgnitionAndEscapeCovariates)
+ #TODO: IE wrote this - please review it
+ if (!is.null(sim$fireSense_IgnitionFitted$rescales)) {
+   for (name in names(sim$fireSense_IgnitionFitted$rescales)) {
+     op <- eval(parse(text = sim$fireSense_IgnitionFitted$rescales[[name]]),
+                env = fireSense_IgnitionCovariates)
+     fireSense_IgnitionCovariates[, eval(name) := op]
+   }
+ }
+
+ ## Toolbox: set of functions used internally by IgnitionPredictRun
+  IgnitionPredictRaster <- function(model, data, sim) {
+    model %>%
+      model.matrix(c(data, sim$knots)) %>%
+      `%*%` (sim$fireSense_IgnitionFitted$coef) %>%
+      drop %>% sim$fireSense_IgnitionFitted$family$linkinv(.) %>%
+      `*` (P(sim)$rescaleFactor)
+  }
 
   ## Handling piecewise terms in a formula
   pw <- function(v, k) pmax(v - k, 0)
 
   # Load inputs in the data container
-  # list2env(as.list(envir(sim)), envir = mod)  
-  mod_env <- new.env(parent = emptyenv())
-  for (x in P(sim)$data) {
-    if (!is.null(sim[[x]])) {
-      if (is.data.frame(sim[[x]])) {
-        list2env(sim[[x]], envir = mod_env)
-      } else if (is(sim[[x]], "RasterStack") || is(sim[[x]], "RasterBrick")) {
-        list2env(setNames(unstack(sim[[x]]), names(sim[[x]])), envir = mod_env)
-      } else if (is(sim[[x]], "RasterLayer")) {
-        mod_env[[x]] <- sim[[x]]
-      } else stop(moduleName, "> '", x, "' is not a data.frame, a RasterLayer, 
-                  a RasterStack or a RasterBrick.")
-    }
-  }
+  #TODO: We can get rid of this 'environment data container' construct
+  mod_env <- new.env(parent = baseenv()) #get access to base R
 
   # Define pw() within the data container
   mod_env$pw <- pw
 
-  terms <- delete.response(terms.formula(sim[[P(sim)$modelObjName]][["formula"]]))
+  terms <- delete.response(terms.formula(sim$fireSense_IgnitionFitted[["formula"]]))
 
+  #TODO: get rid of this mapping line (which is deprecated), kept for reference
   ## Mapping variables names to data
-  if (!is.null(P(sim)$mapping)) {
-    for (i in 1:length(P(sim)$mapping)) {
-      attr(terms, "term.labels") %<>% gsub(
-        pattern = names(P(sim)$mapping[i]),
-        replacement = P(sim)$mapping[[i]],
-        x = .
-      )
-    }
-  }
+  # if (!is.null(P(sim)$mapping)) {
+  #   for (i in 1:length(P(sim)$mapping)) {
+  #     attr(terms, "term.labels") %<>% gsub(
+  #       pattern = names(P(sim)$mapping[i]),
+  #       replacement = P(sim)$mapping[[i]],
+  #       x = .
+  #     )
+  #   }
+  # }
 
   formula_fire <- reformulate(attr(terms, "term.labels"), intercept = attr(terms, "intercept"))
   allxy <- all.vars(formula_fire)
 
-  if (!is.null(sim[[P(sim)$modelObjName]]$knots)) {
-    list2env(as.list(sim[[P(sim)$modelObjName]]$knots), envir = mod_env)
-    kNames <- names(sim[[P(sim)$modelObjName]]$knots)
+  #add knots
+  #TODO: I'm not 100% sure this works with a linear model only
+  if (!is.null(sim$fireSense_IgnitionFitted$knots)){
+    for (knot in names(sim$fireSense_IgnitionFitted$knots)) {
+      fireSense_IgnitionCovariates[, eval(knot) := sim$fireSense_IgnitionFitted$knots[knot]]
+    }
+    kNames <- names(sim$fireSense_IgnitionFitted$knots)
     allxy <- allxy[!allxy %in% kNames]
-  } else kNames <- NULL
+  }
+
+  list2env(fireSense_IgnitionCovariates, env = mod_env)
 
   if (all(unlist(lapply(allxy, function(x) is.vector(mod_env[[x]]))))) {
+
     sim$fireSense_IgnitionPredicted <- (
       formula_fire %>%
         model.matrix(mod_env) %>%
-        `%*%`(sim[[P(sim)$modelObjName]]$coef) %>%
-        drop %>% sim[[P(sim)$modelObjName]]$family$linkinv(.)
+        `%*%`(sim$fireSense_IgnitionFitted$coef) %>%
+        drop %>% sim$fireSense_IgnitionFitted$family$linkinv(.)
     ) %>% `*`(P(sim)$rescaleFactor)
   } else if (all(unlist(lapply(allxy, function(x) is(mod_env[[x]], "RasterLayer"))))) {
     covList <- mget(allxy, envir = mod_env, inherits = FALSE)
     tryCatch({
       raster::stack(covList)
     }, error = function(e){
-      stop("At least one of the covariate rasters does not align with the others. Please debug your inputs. 
+      stop("At least one of the covariate rasters does not align with the others. Please debug your inputs.
               Consider using a function like reproducible::postProcess on your layers to make sure these align.")
     })
-    sim$fireSense_IgnitionPredicted <- raster::stack(covList) %>% 
-      raster::predict(model = formula_fire, fun = frequencyPredictRaster, na.rm = TRUE, sim = sim)
+    sim$fireSense_IgnitionPredicted <- raster::stack(covList) %>%
+      raster::predict(model = formula_fire, fun = IgnitionPredictRaster, na.rm = TRUE, sim = sim)
   } else {
     missing <- !allxy %in% ls(mod_env, all.names = TRUE)
 
@@ -199,11 +173,17 @@ frequencyPredictRun <- function(sim) {
     }
   }
 
+  #force ignitionPredicted a raster
+  if (!class(sim$fireSense_IgnitionPredicted) == "RasterLayer"){
+    IgnitionRas <- raster(sim$flammableRTM)
+    IgnitionRas[fireSense_IgnitionCovariates$pixelID] <- sim$fireSense_IgnitionPredicted
+    sim$fireSense_IgnitionPredicted <- IgnitionRas
+  }
+
   invisible(sim)
 }
 
-frequencyPredictSave <- function(sim)
-{
+IgnitionPredictSave <- function(sim) {
   moduleName <- current(sim)$moduleName
   timeUnit <- timeunit(sim)
   currentTime <- time(sim, timeUnit)
